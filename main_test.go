eb1e80fdc0440a299eb7b85eea543aff61ee998e
benibana2001 <benibana2001@gmail.com>
2019-05-07 11:40:42 +0900

    fix test

--- main_test.go
+++ main_test.go
@@ -23,3 +23,19 @@ func TestSelectUser(t *testing.T) {
 		t.Errorf("Expected user.Email is 'takeru@mail.jp', but got '%v'", user.Email)
 	}
 }
+
+func TestSelectAllUser(t *testing.T) {
+	db, err := connectDb()
+	defer db.Close()
+	if err != nil{
+		fmt.Printf("%v", err)
+	}
+
+	users := selectAllUser(db)
+	if users[0].Id != 1 {
+		t.Errorf("Expected users[0].Id is 1, but got %v", users[0].Id)
+	}
+	if users[2].Id != 3 {
+		t.Errorf("Expected users[2].Id is 3, but got %v", users[2].Id)
+	}
+}
